9ad2b984419ba8190dccc62da886fe4ba7f9068b
unlz <skull31@hotmail.com>
2014-08-01 14:55:39 -0300

    ConstanciaAlumnoRegular: se aplica un trim a los datos de incorporacion y reincorporacion, ya que en BD hay caracteres basura que vienen de los archivos enviados por la facultad

--- InscripcionesCursos/Privado/Alumnos/ConstanciaAlumnoRegular.aspx.cs
+++ InscripcionesCursos/Privado/Alumnos/ConstanciaAlumnoRegular.aspx.cs
@@ -106,7 +106,7 @@ namespace InscripcionesCursos.Privado.Alumnos
                 if (text != ConfigurationManager.AppSettings["DefaultText"])
                 {
                     //Validate if the student has reincorporation or not
-                    if ((user.CuatrimestreAnioIngreso.IndexOf(PrimerCuatrimestre) != -1) && ((user.CuatrimestreAnioReincorporacion) == null || (user.CuatrimestreAnioReincorporacion == string.Empty)))
+                    if ((user.CuatrimestreAnioIngreso.Trim().IndexOf(PrimerCuatrimestre) != -1) && ((user.CuatrimestreAnioReincorporacion.Trim()) == null || (user.CuatrimestreAnioReincorporacion.Trim() == string.Empty)))
                     {
                         if (DateTime.Now.Month <= 3)
                         {
@@ -121,7 +121,7 @@ namespace InscripcionesCursos.Privado.Alumnos
                     }
                     else
                     {
-                        if ((user.CuatrimestreAnioIngreso.IndexOf(SegundoCuatrimestre) != -1) && ((user.CuatrimestreAnioReincorporacion) == null || (user.CuatrimestreAnioReincorporacion == string.Empty)))
+                        if ((user.CuatrimestreAnioIngreso.Trim().IndexOf(SegundoCuatrimestre) != -1) && ((user.CuatrimestreAnioReincorporacion.Trim()) == null || (user.CuatrimestreAnioReincorporacion.Trim() == string.Empty)))
                         {
                             if (DateTime.Now.Month <= 7)
                             {
@@ -136,7 +136,7 @@ namespace InscripcionesCursos.Privado.Alumnos
                         }
                         else
                         {
-                            if (user.CuatrimestreAnioReincorporacion.IndexOf(PrimerCuatrimestre) != -1)
+                            if (user.CuatrimestreAnioReincorporacion.Trim().IndexOf(PrimerCuatrimestre) != -1)
                             {
                                 if (DateTime.Now.Month <= 3)
                                 {
