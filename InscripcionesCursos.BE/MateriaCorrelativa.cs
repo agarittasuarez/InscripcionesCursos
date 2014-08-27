84efefde761949c64935379a8548c2dfc1288636
unlz <skull31@hotmail.com>
2014-08-26 22:34:23 -0300

    Nueva version que incorpora la visualizacion de materias correlativas. Subido por Sergio Vitali

---
+++ InscripcionesCursos.BE/MateriaCorrelativa.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace InscripcionesCursos.BE
+{
+    public class MateriaCorrelativa
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the MateriaCorrelativa class.
+        /// </summary>
+        public MateriaCorrelativa()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the MateriaCorrelativa class.
+        /// </summary>
+        public MateriaCorrelativa(int idCarrera, int idMateria, int idMateriaPredecesora, string estado)
+        {
+            this.IdCarrera = idCarrera;
+            this.IdMateria = idMateria;
+            this.idMateriaPredecesora = idMateriaPredecesora;
+            this.Estado = estado;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the IdCarrera value.
+        /// </summary>
+        public int IdCarrera { get; set; }
+
+        /// <summary>
+        /// Gets or sets the IdMateria value.
+        /// </summary>
+        public int IdMateria { get; set; }
+
+        /// <summary>
+        /// Gets or sets the idMateriaPredecesora value.
+        /// </summary>
+        public int idMateriaPredecesora { get; set; }
+
+        /// <summary>
+        /// Gets or sets the estado value.
+        /// </summary>
+        public string Estado { get; set; }
+
+        #endregion
+    }
+}
---
+++ InscripcionesCursos.DTO/MateriaCorrelativaDTO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InscripcionesCursos.BE;
+using InscripcionesCursos.DAO;
+
+namespace InscripcionesCursos.DTO
+{
+    public static class MateriaCorrelativaDTO
+    {
+        #region Constants
+
+        const string connectionString = "InscripcionesCursos";
+
+        #endregion
+
+        #region Methods
+
+        public static List<MateriaCorrelativa> GetMateriasCorrelativasByUser(int DNI, int idCarrera)
+        {
+            try
+            {
+                MateriaCorrelativaDAO materiasDAO = new MateriaCorrelativaDAO(connectionString);
+                return materiasDAO.MateriasCorrelativasSelectByUser(DNI, idCarrera);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        #endregion
+    }
+}
